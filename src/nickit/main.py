3ea079adc55f5da08a460ca24293da3dadfafe1c
Hector Montoya <hectormontoyadeveloper@gmail.com>
2026-07-10 23:18:51 -0600

    first version of the cli this is just the base structure

---
+++ src/nickit/main.py
@@ -0,0 +1,26 @@
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def greet(name: str, twice: bool = False):
+    """
+    A simple greeting CLI utility using Typer.
+    """
+    greeting = f"Hello, {name}!"
+    typer.echo(greeting)
+    if twice:
+        typer.echo(greeting)
+
+
+@app.command()
+def goodbye(name: str):
+    """
+    Bid farewell to someone.
+    """
+    typer.echo(f"Goodbye, {name}!")
+
+
+def run():
+    app()
